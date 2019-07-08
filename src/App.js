160b3077c6fe524a3b9078d078052a7d061e9c80
Evan McCoy <evan.mccoy@gmail.com>
2019-07-08 13:12:23 -0400

    first commit?

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import * as Data from "./data/data.json";
+
+import Header from "./components/Header";
+import ListLayout from "./components/ListLayout";
+import Footer from "./components/Footer";
+
+import Wrapper from "./sharedComponents/Wrapper";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Wrapper>
+        <ListLayout posts={Data} />
+      </Wrapper>
+      <Footer />
     </div>
   );
 }
---
+++ src/sharedComponents/Wrapper.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  min-height: calc(100vh - 64px);
+  margin: 0 auto;
+  max-width: 60rem;
+`;
+
+export default Wrapper;
